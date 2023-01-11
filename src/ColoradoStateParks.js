d953a7a10f349bc567ff5677092c0e80d0fa5230
LincKim <lincolnkimutai@gmail.com>
2023-01-11 16:02:28 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife, elevation} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  MesaVerde()
+  wildlife()
+  elevation()
+  console.log(trees)
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
